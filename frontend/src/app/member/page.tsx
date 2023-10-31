eec999e98a09442e077fa9009525acf21b25449a
Phan Tan Thang <thangphan@kms-techonology.com>
2023-10-31 23:34:50 +0700

    Fix can not find component

--- frontend/src/app/member/page.tsx
+++ frontend/src/app/member/page.tsx
@@ -1,18 +1,7 @@
-import { RootState } from '@/redux/store';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement, incrementByAmount } from '@/redux/Features/counter/countSlice';
-import DashboardPage from '@/modules/dashboard/DashboardPage';
 
 const AdminDashboard: React.FC = () => {
-  // const count = useSelector((state: RootState) => state.counter.value);
-  // const dispatch = useDispatch();
-
-  return (
-    <main>
-      <DashboardPage />
-    </main>
-  );
+  return <main>Admin Dashboard</main>;
 };
 
 export default AdminDashboard;
