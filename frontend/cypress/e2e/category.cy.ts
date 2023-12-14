10e9c86d573daa0fe941c3706f77fe41e9b66c95
phantanthang <thangphan@kms-techonology.com>
2023-12-14 17:46:09 +0700

    feat: add test E2E for category - sidebar component

---
+++ frontend/cypress/e2e/category.cy.ts
@@ -0,0 +1,42 @@
+context('Category - Sidebar component', () => {
+  const TOTAL_CHILD_CATEGORIES_OF_SNACK_CATEGORY: number = 3;
+  beforeEach(() => {
+    cy.visit('/collections/ca-phe');
+    cy.wait(3000);
+  });
+
+  describe('E2E GUI', () => {
+    let categoryLink: any;
+
+    it('should highlight category with correct color when click on a category', () => {
+      const chakraActivePrimaryHighlightColor: string = 'css-13lp4jc';
+
+      categoryLink = cy.get('a[href="/collections/banh"] > p');
+      categoryLink.click();
+      cy.wait(3000);
+
+      categoryLink.should('have.class', chakraActivePrimaryHighlightColor);
+    });
+
+    it('should display enough child categories when click on a category', () => {
+      categoryLink = cy.get('a[href="/collections/banh"] > p');
+      categoryLink.click();
+
+      const totalChildCategories = cy
+        .get('a[href="/collections/banh"]')
+        .parent('li')
+        .children('div')
+        .find('ul > li')
+        .should('have.length', TOTAL_CHILD_CATEGORIES_OF_SNACK_CATEGORY);
+      cy.wait(3000);
+
+      totalChildCategories.each(($li) => {
+        cy.wrap($li).should('be.visible');
+      });
+    });
+  });
+
+  describe('E2E Function', () => {
+    describe('validate password', () => {});
+  });
+});
